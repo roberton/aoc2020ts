490637935f4039574e9f4ef28d1a3df75cf8f7ae
roberton <rnerici@gmail.com>
2020-12-03 13:17:24 +0000

    refactored day 3 ready to do second part

--- src/day/03.ts
+++ src/day/03.ts
@@ -6,14 +6,7 @@ export function star1 (): void {
   console.log('Day 3, Star 1');
   const map = loadFile(fileName);
 
-  let x = 0;
-  let y = 0;
-  let treeCount = 0;
-  for (let i = 0; i < map.length - 1; i++) {
-    x += 3;
-    y += 1;
-    if (isTreeAt(map, x, y)) treeCount++;
-  }
+  const treeCount = countTrees(map, 3, 1);
 
   console.log(`Number of trees encountered = ${treeCount}`);
 }
@@ -26,4 +19,17 @@ export function isTreeAt (map: string[], x: number, y: number): boolean {
   const mapWidth = map[0].length;
   const xPosInMap = x % mapWidth;
   return map[y][xPosInMap] === '#';
+}
+
+export function countTrees (map: string[], slopeX: number, slopeY: number): number {
+  let x = 0;
+  let y = 0;
+  let treeCount = 0;
+  for (let i = 0; i < map.length - 1; i++) {
+    x += slopeX;
+    y += slopeY;
+    if (isTreeAt(map, x, y)) treeCount++;
+  }
+
+  return treeCount;
 }
--- tests/03.test.ts
+++ tests/03.test.ts
@@ -1,4 +1,4 @@
-import { isTreeAt } from '../src/day/03';
+import { isTreeAt, countTrees } from '../src/day/03';
 
 const testMap = [
   '..##.......',
@@ -18,16 +18,10 @@ describe('isTreeAt', () => {
   it('should return false for first test example', () => {
     expect(isTreeAt(testMap, 3, 1)).toBe(false);
   });
+});
 
+describe('countTrees', () => {
   it('should find 7 tree in full test example', () => {
-    let x = 0;
-    let y = 0;
-    let treeCount = 0;
-    for (let i = 0; i < testMap.length - 1; i++) {
-      x += 3;
-      y += 1;
-      if (isTreeAt(testMap, x, y)) treeCount++;
-    }
-    expect(treeCount).toBe(7);
+    expect(countTrees(testMap, 3, 1)).toBe(7);
   });
 });
