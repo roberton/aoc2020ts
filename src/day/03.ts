40823d9fcdb29b5270507f07c750b72ed52f846e
roberton <rnerici@gmail.com>
2020-12-03 13:23:54 +0000

    implements day 3, star 2

--- src/day/03.ts
+++ src/day/03.ts
@@ -5,7 +5,6 @@ const fileName = 'src/day/03.txt';
 export function star1 (): void {
   console.log('Day 3, Star 1');
   const map = loadFile(fileName);
-
   const treeCount = countTrees(map, 3, 1);
 
   console.log(`Number of trees encountered = ${treeCount}`);
@@ -13,6 +12,16 @@ export function star1 (): void {
 
 export function star2 (): void {
   console.log('Day 3, Star 2');
+  const map = loadFile(fileName);
+
+  const treeCount: number[] = [];
+  treeCount[0] = countTrees(map, 1, 1);
+  treeCount[1] = countTrees(map, 3, 1);
+  treeCount[2] = countTrees(map, 5, 1);
+  treeCount[3] = countTrees(map, 7, 1);
+  treeCount[4] = countTrees(map, 1, 2);
+
+  console.log(treeCount);
 }
 
 export function isTreeAt (map: string[], x: number, y: number): boolean {
@@ -25,7 +34,7 @@ export function countTrees (map: string[], slopeX: number, slopeY: number): numb
   let x = 0;
   let y = 0;
   let treeCount = 0;
-  for (let i = 0; i < map.length - 1; i++) {
+  while ((y + slopeY) < map.length) {
     x += slopeX;
     y += slopeY;
     if (isTreeAt(map, x, y)) treeCount++;
